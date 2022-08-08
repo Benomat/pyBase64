a93323dec5dbe18213510045ec41885d0777d529
Benomat <71938735+Benomat@users.noreply.github.com>
2022-08-08 18:49:38 +0200

    Update base64encode.py

--- base64encode.py
+++ base64encode.py
@@ -1,7 +1,7 @@
 import base64
 from pyperclip import copy
 
-message = open("hash.py", "r",encoding='utf-8').read()
+message = open("<Path to file>", "r",encoding='utf-8').read()
 message_bytes = message.encode('utf-8')
 base64_bytes = base64.b64encode(message_bytes)
 base64_message = base64_bytes.decode('ascii')
